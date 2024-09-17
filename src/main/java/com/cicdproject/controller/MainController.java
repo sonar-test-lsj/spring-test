6e319c051eb193cfd53e4540cb6c56ea14c273c1
masiljangajji <xmfpdlsj0508@gmail.com>
2024-09-18 03:13:54 +0900

    init

---
+++ src/main/java/com/cicdproject/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.cicdproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "CICD test";
+	}
+
+	@GetMapping("/")
+	public String index(){
+
+
+		try {
+			// 서버의 호스트 이름과 IP 주소를 가져옴
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			String serverIp = inetAddress.getHostAddress(); // 서버의 IP 주소
+			return "Server IP Address: " + serverIp;
+		} catch (UnknownHostException e) {
+			return "Unable to retrieve server IP address";
+		}
+
+	}
+
+
+}
